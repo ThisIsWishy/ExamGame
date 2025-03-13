d505cd78ac4e803c6e16b411073347bac33a34f7
ThisIsWishy <williamjungbilleolsen@gmail.com>
2025-03-13 09:55:20 +0100

    Movement Improvement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float jumpPower;
     [SerializeField] private float speed;
-    private Rigidbody2D body;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        body.linearVelocity = new UnityEngine.Vector2(Input.GetAxis("Horizontal") * speed, 0);
-
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            body.linearVelocity = new UnityEngine.Vector2(body.linearVelocity.x, speed);
+            rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, jumpPower);
         }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+    void FixedUpdate()
+    {
+       rb.linearVelocity = new UnityEngine.Vector2(Input.GetAxis("Horizontal")*speed, rb.linearVelocity.y);
     }
 }
